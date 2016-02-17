36a7978c7fa36e6047d9cc0da24437d6b59c0d9b
Andreas Lüdeke <aluedeke@testobject.com>
2016-02-17 11:38:19 +0100

    ...

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ mv /root/node_modules/appium /root/appium/appium/$APPIUM_VERSION
 mkdir /root/appium/appium/$APPIUM_VERSION/build/SafariLauncher
 cp --no-clobber /root/SafariLauncher.zip /root/appium/appium/$APPIUM_VERSION/build/SafariLauncher/SafariLauncher.zip
 rm /root/SafariLauncher.zip
-if "$APPIUM_VERSION" == "1.4.16" ; then
+if [ "$APPIUM_VERSION" == "1.4.16" ]; then
   echo "replacing chromium driver"
   cd /root/appium/appium/$APPIUM_VERSION
   npm install appium-chromedriver@2.5.1
