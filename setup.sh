d2f3c6c1807106096c45c17376effa9f45fcbf59
Andreas Lüdeke <aluedeke@testobject.com>
2016-02-29 13:21:47 +0100

    fix dumpsys buffer overflow in appium 1.4.16

--- setup.sh
+++ setup.sh
@@ -12,5 +12,6 @@ if [ "$APPIUM_VERSION" == '1.4.16' ]; then
   echo "replacing chromium driver"
   cd /root/appium/appium/$APPIUM_VERSION
   npm install appium-chromedriver@2.5.1
-  patch -p0 < /root/patches/chrome.patch;
+  patch -p0 < /root/patches/0001-fix-BufferOverflow-by-limiting-output-of-dumpsys.patch;
+  patch -p0 < /root/patches/0002-handover-custom-adb-port-to-chromedriver.patch;
 fi
