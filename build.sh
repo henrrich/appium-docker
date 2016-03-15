41d7aff2d6862f9648ab6f43cd8a955757a22f13
Andreas Lüdeke <aluedeke@testobject.com>
2016-03-15 14:17:02 +0100

    fix build

--- build.sh
+++ build.sh
@@ -4,7 +4,5 @@ tag=$2
 
 docker build --build-arg APPIUM_VERSION=$appium_version -t testobject-appium .
 
-docker build -t testobject-appium .
-
 docker tag -f testobject-appium testobject/appium:$appium_version
 docker push testobject/appium:$appium_version
