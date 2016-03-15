d5b551cf565529110b11271051e48ce4e00e1c70
Andreas Lüdeke <aluedeke@testobject.com>
2016-03-15 14:03:05 +0100

    push to docker hub

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ echo "APPIUM_VERSION=$appium_version" > version.env
 
 docker build -t testobject-appium .
 
-docker tag -f testobject-appium head.testobject.org:5000/testobject-appium:$appium_version
-docker push head.testobject.org:5000/testobject-appium:$appium_version
+docker tag -f testobject-appium testobject/appium:$appium_version
+docker push testobject/appium:$appium_version
